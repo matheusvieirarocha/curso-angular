b50af62ae2ffe3ce653a970bc7a261048037d4d1
Matheus Rocha <professormatheusvieira@gmail.com>
2019-11-08 00:10:03 -0300

    secao 3 aula 13

--- Aula_03-11/app.ts
+++ Aula_03-11/app.ts
@@ -1,41 +1,11 @@
-class Spacecraft {
-    constructor(public propulsor: string){
-
-    }
-
-    jumpIntoHyperspace(){
-        console.log(`Entering hyperspace with ${this.propulsor}`)
-    }
-}
+import { Spacecraft, Containership } from "./base-ships"
+import { MilleniumFalcon } from "./starfighters"
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
-class MilleniumFalcon extends Spacecraft implements Containership{
-    
-    cargoContainers: number
-    
-    constructor(){
-        super('hyperdrive')
-        this.cargoContainers = 4
-    }
-
-    jumpIntoHyperspace(){
-        if(Math.random() >= 0.5){
-            super.jumpIntoHyperspace()
-        }else{
-            console.log("Failed to jump into Hyperspace")
-        }
-    }
-}
-
 let falcon = new MilleniumFalcon
 falcon.jumpIntoHyperspace()
-
-interface Containership{
-
-    cargoContainers: number
-}
 
 let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2
 console.log(`is falcon good for the job? ${goodForTheJob(falcon) ? 'yes': 'no'}`)
